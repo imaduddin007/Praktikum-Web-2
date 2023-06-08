adba998235b59a8787831f4bbf04b8118f0aec98
imaduddin007 <imaduddinabdurrahman01@gmail.com>
2023-06-08 15:44:20 +0700

    Tugas Praktikum 12

--- praktikum10/routes/web.php
+++ praktikum10/routes/web.php
@@ -29,3 +29,13 @@ Route::get('/produk', [ProdukController::class, 'index']);
 Route::get('/home', [FrontendController::class, 'index']);
 
 Route::get('/about', [AboutController::class, 'index']);
+
+Route::get('/produk/create', [ProdukController::class, 'create']);
+
+Route::post('/produk/store', [ProdukController::class, 'store']);
+
+Route::get('produk/edit/{id}', [ProdukController::class, 'edit']);
+
+Route::put('/produk/update/{id}', [ProdukController::class, 'update']);
+
+Route::get('/produk/delete/{id}', [ProdukController::class, 'destroy']);
